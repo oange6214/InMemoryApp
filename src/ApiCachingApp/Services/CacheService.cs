83a5749e7181524f139191a171631069f5f87c24
Pohan <oange6214@gmail.com>
2024-02-28 19:02:27 +0800

    feat: 新增一個 Cache service，可以再應用程式中使用 Memory Cache 來儲存和擷取資料。

---
+++ src/ApiCachingApp/Services/CacheService.cs
@@ -0,0 +1,62 @@
+
+using System.Runtime.Caching;
+
+namespace ApiCachingApp.Services;
+
+public class CacheService : ICacheService
+{
+    private ObjectCache _memoryCache = MemoryCache.Default;
+
+    public T GetData<T>(string key)
+    {
+        try
+        {
+            T item = (T) _memoryCache.Get(key);
+            return item;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+
+    public object RemoveData(string key)
+    {
+        var res = true;
+
+        try
+        {
+            if (!string.IsNullOrEmpty(key))
+            {
+                var result = _memoryCache.Remove(key);
+            }            
+            else
+                res = false;
+
+            return res;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+
+    public bool SetData<T>(string key, T value, DateTimeOffset expirationTime)
+    {
+        var result = true;
+
+        try
+        {
+            if (!string.IsNullOrEmpty(key))
+                _memoryCache.Set(key, value, expirationTime);
+            else
+                result = false;
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+}
